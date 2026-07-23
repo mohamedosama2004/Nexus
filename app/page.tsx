36548e04b6df028ea1a080781ff47842d6a66506
Mohamed Osama <146884487+mohamedosama2004@users.noreply.github.com>
2026-07-23 18:13:05 +0300

    initial landing page

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <main>
+     <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">
+              Nexus
+            </h1>
+            <p className="py-6">
+              Project Management Saas
+            </p>
+             <p>
+                Manag projects,tasks,and teams in one place
+              </p>
+            <button className="btn btn-primary">
+              Get started
+            </button>
+          </div>
+        </div>
+     </div>
+    </main>
+  );
+}
