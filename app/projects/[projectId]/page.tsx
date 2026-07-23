21115f514f52c91bcd2a3dabe22a1071d0113880
Mohamed Osama <146884487+mohamedosama2004@users.noreply.github.com>
2026-07-23 21:37:42 +0300

    feat: add routing and navigation

---
+++ app/projects/[projectId]/page.tsx
@@ -0,0 +1,13 @@
+type ProjectPageProps={
+    params : Promise<{projectId:string}>
+}
+
+
+export default async function ProjectPage({params}:ProjectPageProps) {
+    const {projectId}= await params ;
+    return (
+        <div>
+            project : {projectId}
+        </div>
+    )
+}
