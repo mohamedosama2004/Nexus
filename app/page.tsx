4e20b02ed8ed1a758ed8378ef86ed41a23c55446
Mohamed Osama <146884487+mohamedosama2004@users.noreply.github.com>
2026-07-23 18:49:46 +0300

    feat : creating landing page branch

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ export default function Home() {
     <main>
      <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content text-center">
-          <div className="max-w-md">
+          <div className="max-w-md space-y-5">
             <h1 className="text-5xl font-bold">
               Nexus
             </h1>
-            <p className="py-6">
+            <p className="font-bold">
               Project Management Saas
             </p>
              <p>
-                Manag projects,tasks,and teams in one place
+                Manage projects, tasks , and teams in one place
               </p>
             <button className="btn btn-primary">
               Get started
